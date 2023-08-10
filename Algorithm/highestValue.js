ea8be8f05c2190bc272c35cc7879ce5329b76d6f
Karoline Cruz <karollcruz@hotmail.com>
2023-08-10 23:58:12 +0100

    Find the object with the highest sum of values in its properties. Suggested by Chat GPT.

--- Algorithm/highestValue.js
+++ Algorithm/highestValue.js
@@ -1,24 +1,22 @@
 // Find the object with the highest sum of values in its properties.
 // const highest = arrayMap.reduce((a, b) => Math.max(a, b)); 
-
 const array = [
-    {a: 1, b: 2, c: 3}, 
-    {a: 4, b: 5, c: 6},
-    {a: 7, b: 8, c: 9},
+  {a: 1, b: 2, c: 3}, 
+  {a: 4, b: 5, c: 6},
+  {a: 7, b: 8, c: 9},
 ];
 
 const arrayMap = array.map(obj => obj.a + obj.b + obj.c);
 let highest = 0;
-
 const highestValue =(parameter)=>{
-    for (let i = 0; i < parameter.length; i++) {
-        if (parameter[i] > highest) {
-            highest = parameter[i];
-        }else {
-            highest = highest;
-        }        
-    }
-    return highest;
+  for (let i = 0; i < parameter.length; i++) {
+    if (parameter[i] > highest) {
+      highest = parameter[i];
+    }else {
+      highest = highest;
+    }        
+  }
+  return highest;
 }
 
 console.log(highestValue(arrayMap));
