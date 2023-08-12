e5f95fb77b935a8494bbd2104cc83329c49615f2
Karoline Cruz <karollcruz@hotmail.com>
2023-08-12 15:21:58 +0100

    Beginning of the name draw exercise proposed by the Wdev channel.

--- Algorithm/nameDraw/script.js
+++ Algorithm/nameDraw/script.js
@@ -1,14 +1,39 @@
-let names = ['Beth', 'Eliza', 'John', 'Clemens', 'Martin', 'Cheryl'];
+let names = ['Beth', 'Clemens', 'Fred', 'Angela', 'Carmen'];
 
-if (names.length >= 1) {
-  document.querySelector('.js-list').innerHTML = `List: ${names}`;
+function addName() {
+  const container = document.querySelector('.container');
+
+  const addMessage = document.createElement('p');
+  addMessage.textContent = 'Enter at least two names to be drawn.';
+  container.appendChild(addMessage);
+
+  const inputElement = document.createElement('input');
+  inputElement.type = 'text';
+  inputElement.placeholder = 'Type a name';
+  inputElement.classList.add('input-name');
+  container.appendChild(inputElement);
+
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Add';
+  inputElement.classList.add('add-name');
+  container.appendChild(addButton);
 }
 
-for (let i = 0; i < names.length; i++) {
-  let drawnName = Math.random(names[i]);
-  if (names.length > 1) {
-    document.querySelector('.js-drawn').innerHTML = `The name drawn was ${names}`;
+function listNames() {
+  if (names.length >= 1) {
+    document.querySelector('.js-list').innerHTML = `List: ${names}`;
+  } else {
+    document.querySelector('.js-list').innerHTML = 'List is empty, add at least one name to show the list.'; 
   }
 }
 
+function drawName(names) {
+  if (names.length < 2) {
+    document.querySelector('.js-draw').innerHTML = 'Add at least two names to drawn.';
+  } else {
+    const randomIndex = Math.floor(Math.random() * names.length);
+
+    document.querySelector('.js-draw').innerHTML = `The winner was ${names[randomIndex]}, congrats!`
+  }
+}
 
