77d366e6ac154079d4b729e74f9dd5a3eebb0b22
Karoline Cruz <karollcruz@hotmail.com>
2023-09-25 21:06:49 +0100

    Js improvement. Still working on the code.

--- hangman/script.js
+++ hangman/script.js
@@ -1,5 +1,18 @@
+function guessLetter(){
 let word = "apricot";
+let word2 = "a";
+const wordArray = word.split("");
+let result = [];
 
-for (i = 0; i < word.length; i++){
-  console.log(word[i]);
-}
+  for (i = 0; i < wordArray.length; i++){
+    if (word2 !== wordArray[i]){
+      result.push("_");
+    } else {
+      result.push(word2);
+    }
+  }
+
+  console.log(result);
+ }
+
+ guessLetter();
