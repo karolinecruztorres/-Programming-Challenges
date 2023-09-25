49174267de30bb5d432b473a696368740b0647c0
Karoline Cruz <karollcruz@hotmail.com>
2023-09-25 12:56:49 +0100

    A Tic tac toe game that shows the result on console. Code by 'Programamos um GAME com JavaScript Puro em 28 minutos // Mão no Código #10'. Link: https://www.youtube.com/watch?v=M258B1b_pMs&t=1426s.

--- ticTacToe/script.js
+++ ticTacToe/script.js
@@ -1,113 +1,76 @@
-let game = [];
-let board = [];
-let whoPlays = 0; // 0 = player   1 = pc
-let verify;
-let playing = true;
-let level = 1;
-let pcMove = 1;
-let whoStarts = 1;
- 
-function play(p) {
-  if(playing && whoPlays == 0) {
-    switch (p) {
-      case 1:
-        if(game[0],[0] == ""){
-          game[0],[0] = "x";
-          whoPlays = 1;
-        }
-      break;
-    
-      case 2:
-        if(game[0],[1] == ""){
-          game[0],[1] = "x";
-          whoPlays = 1;
-        }
-      break;
-      
-      case 3:
-        if(game[0],[2] == ""){
-          game[0],[2] = "x";
-          whoPlays = 1;
-        }
-      break;
-    
-      case 4:
-        if(game[1],[0] == ""){
-          game[1],[0] = "x";
-          whoPlays = 1;
-        }
-      break;
-
-      case 5:
-        if(game[1],[1] == ""){
-          game[1],[1] = "x";
-          whoPlays = 1;
-        }
-      break;
-    
-      case 6:
-        if(game[1],[2] == ""){
-          game[1],[2] = "x";
-          whoPlays = 1;
-        }
-      break;
-
-      case 7:
-        if(game[2],[0] == ""){
-          game[2],[0] = "x";
-          whoPlays = 1;
-        }
-      break;
-
-      case 8:
-        if(game[2],[1] == ""){
-          game[2],[1] = "x";
-          whoPlays = 1;
-        }
-      break;
-    
-      default:
-        if(game[2],[2] == ""){
-          game[2],[2] = "x";
-          whoPlays = 1;
-        }
-      break;
+const tic_tac_toe = {
+  board: ["","","","","","","","",""],
+  simbols: {
+    options: ["x", "o"],
+    turn_index: 0,
+    change: function(){
+      this.turn_index = (this.turn_index === 0 ? 1 : 0);
     }
-    updateBoard();
-  }
-}
+  },
+  container_element: null,
+  gameOver: false, 
+  winning_sequences: [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ],
 
-function updateBoard() {
-  for(let l = 0; l < 3; l++){
-    for(let c = 0; c < 3; c++){
-      if(game[l][c]=="x"){
-        board[l][c].innerHTML = "x";
-        board[l][c].style.cursor = "defaut";
-      }else if(game[l][c]=="o"){
-        board[l][c].innerHTML = "o";
-        board[l][c].style.cursor = "defaut";
-      }else{
-        board[l][c].innerHTML = "";
-        board[l][c].style.cursor = "pointer";
+  init: function(container){
+    this.container_element = container;
+  },
+
+  move: function(position){
+    if(this.gameOver) return false;
+    if(this.board[position] === ""){
+       this.board[position] = this.simbols.options[this.simbols.turn_index];
+       this.draw();
+       let winning_sequences_index = this.check_winning_sequences(this.simbols.options[this.simbols.turn_index]);
+       if(winning_sequences_index >= 0){
+        this.game_is_over();
+       }else{
+        this.simbols.change();
+       }
+       return true;
+    }else{
+      return false;
+    }
+  },
+
+  game_is_over: function(){
+    this.gameOver = true;
+    console.log("GAME OVER");
+  },
+
+  start: function(){
+    this.board.fill("");
+    this.draw();
+    this.gameOver = false;
+  },
+
+  check_winning_sequences: function(simbol){
+    for(i in this.winning_sequences){
+      if(this.board[this.winning_sequences[i][0]] == simbol &&
+         this.board[this.winning_sequences[i][1]] == simbol &&
+         this.board[this.winning_sequences[i][2]] == simbol){
+          console.log("Sequencia vencedora: " + i);
+          return i;
       }
-    }
+    };
+    return -1;
+  },
+
+  draw: function(){
+    let content = "";
+
+    for(i in this.board){
+      content += "<div onclick='tic_tac_toe.move(" + i + ")'>" + this.board[i] + "</div>";
+    };
+
+    this.container_element.innerHTML = content;
   }
-}
-
-function start() {
-  playing = true;
-  pcMove = 1;
-  game = [
-         ["","",""],
-         ["","",""],
-         ["","",""]
-  ];
-
-  board = [
-    [document.getElementById("p1"),document.getElementById("p2"),document.getElementById("p3")],
-    [document.getElementById("p4"),document.getElementById("p5"),document.getElementById("p6")],
-    [document.getElementById("p7"),document.getElementById("p8"),document.getElementById("p9")]
-  ];
-}
-
-window.addEventListener("load", start);
+};
