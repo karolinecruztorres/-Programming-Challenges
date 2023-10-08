a393f18b36c811e68a83e739633f7a180151270c
Karoline Cruz <karollcruz@hotmail.com>
2023-10-08 14:01:33 +0100

    Algorithm that reverses the order of letters in words.

--- Algorithms/reverseWords.js
+++ Algorithms/reverseWords.js
@@ -1,13 +1,11 @@
 function reverseWords(sentence) {
-  const words = sentence.split(' ');
-  const reversedWords = words.reverse();
-  const reversedSentence = reversedWords.join(' ');
-  return reversedSentence;
+  const words = sentence.split("").reverse().join("");
+  return words;
 }
 
 const sentence1 = "Hello World";
 const sentence2 = "Coding is fun";
-const sentence3 = "Challenge accepted";
+const sentence3 = "Challenge accepted"; 
 
 console.log(reverseWords(sentence1));  
 console.log(reverseWords(sentence2));  
