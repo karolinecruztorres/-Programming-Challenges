289508d71052d21410d8bd300dd9d20637e67caf
Karoline Cruz <karollcruz@hotmail.com>
2023-10-08 13:51:30 +0100

    Algorithm that concatenates values from an array to form the largest possible number

--- Algorithms/highestCombination.js
+++ Algorithms/highestCombination.js
@@ -1,16 +1,4 @@
-/* Dado um array de inteiros não negativos, encontre o maior número possível formado pela concatenação de todos os elementos do array.
-
-Por exemplo, dada a entrada [50, 2, 1, 9], o maior número possível que pode ser formado é 95021.
-
-A solução deve ter complexidade de tempo O(n log n), ou seja, deve ser uma solução eficiente.
-
-*/
-
-//Primeiro precisa ordenar o array pelo primeiro número de cara valor
-// Depois concatenar usando map
-
 let numbers = [30, 7, 5, 9];
-numbers.sort((a,b) => b - a);
+let sortedNumbers = numbers.map(String).sort((a, b) => b.localeCompare(a)).join("");
 
-console.log(numbers);
-
+console.log(`The highest combination of ${numbers} is '${sortedNumbers}'.`);
