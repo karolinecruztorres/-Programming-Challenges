b070569078cf28470fb52edab0c5764d66fdc1f9
Karoline Cruz <karollcruz@hotmail.com>
2023-10-08 21:38:55 +0100

    Reformulation of the algorithm

--- Algorithms/currentDayTime.js
+++ Algorithms/currentDayTime.js
@@ -3,9 +3,9 @@ function displayCurrentDateTime() {
   const currentDate = new Date();
   const dayOfWeek = daysOfWeek[currentDate.getDay()];
   const time = currentDate.toLocaleTimeString();
-  console.log(currentDate);
+  console.log(currentDate); 
   console.log(`Today is ${dayOfWeek}`);
   console.log(`Current time is ${time}`);
 }
 
- displayCurrentDateTime();
+displayCurrentDateTime();
