99053736652c880863249002852955d26891c1cf
Karoline Cruz <karollcruz@hotmail.com>
2023-10-08 18:11:58 +0100

    Reformulation of the algorithm that searches for an object with the highest value in an array

--- Algorithms/highestValue.js
+++ Algorithms/highestValue.js
@@ -1,22 +1,11 @@
-// Find the object with the highest sum of values in its properties.
-// const highest = arrayMap.reduce((a, b) => Math.max(a, b)); 
-const array = [
+const numbers = [
   {a: 1, b: 2, c: 3}, 
   {a: 4, b: 5, c: 6},
   {a: 7, b: 8, c: 9},
 ];
 
-const arrayMap = array.map(obj => obj.a + obj.b + obj.c);
-let highest = 0;
-const highestValue =(parameter)=>{
-  for (let i = 0; i < parameter.length; i++) {
-    if (parameter[i] > highest) {
-      highest = parameter[i];
-    }else {
-      highest = highest;
-    }        
-  }
-  return highest;
-}
+let numbersSum = numbers.map(obj => obj.a + obj.b + obj.c);
+let highest = numbersSum.reduce((a, b) => Math.max(a, b));
+let maxIndex = numbersSum.indexOf(Math.max(...numbers.map(obj => obj.a + obj.b + obj.c)));
 
-console.log(highestValue(arrayMap));
+console.log(`The object with the highest value is the ${maxIndex + 1}º, with ${highest} as a sum.`);
