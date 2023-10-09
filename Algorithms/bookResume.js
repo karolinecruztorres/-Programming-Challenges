fd1b67a98769dbdf7da76ad64ca9070c30ca25ab
Karoline Cruz <karollcruz@hotmail.com>
2023-10-09 22:18:09 +0100

    Property with review and its average added to the bookResume function.

--- Algorithms/bookResume.js
+++ Algorithms/bookResume.js
@@ -1,12 +1,27 @@
 const book = {
-  title: "The Lord of Rings",
+  title: "The Lord of the Rings",
   author: "J.R.R. Tolkien",
   pages: 1170,
   publish: 1954,
+  reviewer: [
+    { name: "John", rate: 8 },
+    { name: "Margot", rate: 6 },
+    { name: "Carlos", rate: 9 },
+  ]
 };
 
 function bookResume(parameter) { 
-  console.log(`The book "${parameter.title}" was written by ${parameter.author} and publish in ${parameter.publish}. It has ${parameter.pages} pages.`);  
+  let resume = [];
+  let soma = 0;
+  
+  for (let i = 0; i < parameter.reviewer.length; i++) {
+    resume.push(` ${parameter.reviewer[i].name} with ${parameter.reviewer[i].rate}`);
+    soma += parameter.reviewer[i].rate; 
+  }
+  
+  const media = Math.round(soma / parameter.reviewer.length);
+  
+  console.log(`The book "${parameter.title}" was written by ${parameter.author} and publish in ${parameter.publish}. It has ${parameter.pages} pages and recived evaluation from reviewers:${resume}. The average reviewer score is ${media}.`);  
 }
 
 bookResume(book); 
