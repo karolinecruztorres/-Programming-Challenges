c86a209c622bfe2fcc9111c94dd54b4ad9028186
Karoline Cruz <karollcruz@hotmail.com>
2023-10-10 10:53:06 +0100

    Update property ratings.

--- Algorithms/bookResume.js
+++ Algorithms/bookResume.js
@@ -4,24 +4,35 @@ const book = {
   pages: 1170,
   publish: 1954,
   reviewer: [
-    { name: "John", rate: 8 },
-    { name: "Margot", rate: 6 },
-    { name: "Carlos", rate: 9 },
+    { name: "John", ratings: { plot: 8, characters: 9, writing: 7 } },
+    { name: "Margot", ratings: { plot: 6, characters: 8, writing: 9 } },
+    { name: "Carlos", ratings: { plot: 9, characters: 7, writing: 8 } },
   ]
 };
 
 function bookResume(parameter) { 
-  let resume = [];
-  let soma = 0;
+  let review = [];
+  let totalPlot = 0;
+  let totalCharacters = 0;
+  let totalWriting = 0;
   
   for (let i = 0; i < parameter.reviewer.length; i++) {
-    resume.push(` ${parameter.reviewer[i].name} with ${parameter.reviewer[i].rate}`);
-    soma += parameter.reviewer[i].rate; 
+    review.push(`- ${parameter.reviewer[i].name}: Plot (${parameter.reviewer[i].ratings.plot}), Characters (${parameter.reviewer[i].ratings.characters}), Writing (${parameter.reviewer[i].ratings.writing})\n`);
+    totalPlot += parameter.reviewer[i].ratings.plot; 
+    totalCharacters += parameter.reviewer[i].ratings.characters; 
+    totalWriting += parameter.reviewer[i].ratings.writing; 
   }
   
-  const average = Math.round(soma / parameter.reviewer.length);
+  const averagePlot = Math.round(totalPlot / parameter.reviewer.length);
+  const averageCharacters = Math.round(totalCharacters / parameter.reviewer.length);
+  const averageWriting = Math.round(totalWriting / parameter.reviewer.length);
   
-  console.log(`The book "${parameter.title}" was written by ${parameter.author} and publish in ${parameter.publish}. It has ${parameter.pages} pages and recived evaluation from reviewers:${resume}. The average reviewer score is ${average}.`);  
+  console.log(`The book "${parameter.title}" was written by ${parameter.author}, publish in ${parameter.publish} and has ${parameter.pages} pages. Here are the evaluation from reviewers:
+${review.join("")} 
+ The average reviewer scores are: 
+ - Plot: ${averagePlot}
+ - Characters: ${averageCharacters} 
+ - Writing: ${averageWriting}`);  
 }
 
 bookResume(book); 
