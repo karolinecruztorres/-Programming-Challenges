da26e453c4e3f141e5924fc50fd646926bde73d3
Karoline Cruz <karollcruz@hotmail.com>
2023-10-12 13:06:37 +0100

    Function to call the loops.

--- Algorithms/calculateAverage.js
+++ Algorithms/calculateAverage.js
@@ -1,15 +1,20 @@
 const students = [
   { name: "Alice", age: 20, grades: [85, 90, 92] },
   { name: "Bob", age: 22, grades: [88, 87, 84] },
-  { name: "Charlie", age: 21, grades: [95, 89, 91] }
+  { name: "Maia", age: 21, grades: [95, 89, 91] }
 ];
 
-for (let i = 0; i < students.length; i++) {
-  let sum = 0;
-  for (let j = 0; j < students[i].grades.length; j++) {
-    sum += students[i].grades[j];
+function calculateAverage(students) {
+  for (let i = 0; i < students.length; i++) {
+    let total = 0;
+    for (let j = 0; j < students[i].grades.length; j++) {
+      total += students[i].grades[j];
+    }
+  
+    const average = parseFloat((total / students[i].grades.length).toFixed(2));
+    students[i] = {...students[i], gradeAverage: average};    
   }
-
-  const average = Math.floor(sum / students[i].grades.length);
-  console.log(`${students[i].name}: Média das notas = ${average}`);
+  console.log(students);
 }
+
+calculateAverage(students);
