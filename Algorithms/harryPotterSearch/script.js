efca363c4716ee3dd8169af7ecbaded8e93f5ac4
Karoline Cruz <karollcruz@hotmail.com>
2023-11-11 20:00:12 +0000

    Layout created with CSS, separation of functions and creation of a function that populates the select with data from the api.

--- Algorithms/harryPotterSearch/script.js
+++ Algorithms/harryPotterSearch/script.js
@@ -1,18 +1,62 @@
-async function callApi() {
+async function fetchData() {
   const response = await fetch("https://hp-api.onrender.com/api/characters");
   const data = await response.json();
   return data;
 };
 
-document.querySelector("#characterForm").addEventListener("submit", (e) => {
+//.catch(error => console.error('Error fetching data from API:', error));
+
+document.getElementById("characterForm").addEventListener("submit", (e) => {
   e.preventDefault();
-  const inputValue = document.querySelector("#dropdown").value;
+  const inputValue = document.getElementById("dropdown").value;
   charactersInfo(inputValue);
 });
 
 function charactersInfo(inputValue) {
-  callApi().then((data) => {
+  fetchData().then((data) => {
     const result = data.filter(item => item.name.toLowerCase().includes(inputValue.toLowerCase()));
-      console.log("ok");
+    console.log(inputValue);
+  });
+}
+
+function selectList(value) {
+  console.log(value);
+
+  if (value === 'name') {
+    document.querySelector("#charRadio").src = "images/black_icon.png";
+    document.querySelector("#actorRadio").src = "images/goden_icon.png";
+    
+    fetchData().then(data => populateSelectChar(data)); 
+
+  } else if (value === 'actor') {
+    document.querySelector("#actorRadio").src = "images/black_icon.png";
+    document.querySelector("#charRadio").src = "images/goden_icon.png";
+
+    fetchData().then(data => populateSelectActor(data)); 
+  }
+};
+
+const select = document.getElementById("dropdown");
+select.innerHTML = '';
+
+const defaultOption = document.createElement("option");
+defaultOption.text = "Select the one you search for";
+select.add(defaultOption);  
+
+function populateSelectChar(data) {
+  data.forEach(item => {
+    const option = document.createElement("option");
+    option.text = item.name;
+    option.value = item.name;
+    select.add(option);
+  });
+}
+
+function populateSelectActor(data) {
+  data.forEach(item => {
+    const option = document.createElement("option");
+    option.text = item.actor;
+    option.value = item.actor;
+    select.add(option);
   });
 }
