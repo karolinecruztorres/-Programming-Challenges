819b9245493a1f95027f1704a12abdf92734df23
Karoline Cruz <karollcruz@hotmail.com>
2023-12-09 10:22:37 +0000

    Code correction.

--- Algorithms/smallAlgorithms/sumOfSubsets.js
+++ Algorithms/smallAlgorithms/sumOfSubsets.js
@@ -1,8 +1,8 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function returnNumbers(array, target) {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       if (array[i] + array[j] === target) {
         console.log(`${array[i]} + ${array[j]} = ${array[i] + array[j]}`);
       }
@@ -11,7 +11,3 @@ function returnNumbers(array, target) {
 }
 
 returnNumbers(numbers, 10);
-// 1 + 9
-// 2 + 8
-// 4 + 6
-// 3 + 7
